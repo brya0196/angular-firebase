8a362bb500375de4060d31dc519f61e575cb0754
MOTORCREDITO\bastacio <b.astacio@motorcredito.com.do>
2018-04-10 11:49:18 -0700

    added the logic to add product to the firebase app and reset the form

--- src/app/componets/products/product/product.component.ts
+++ src/app/componets/products/product/product.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+//service
+import { ProductService } from "../../../services/product.service";
+
+//product class
+import { Product } from '../../../models/product';
+
 
 @Component({
   selector: 'app-product',
@@ -7,9 +15,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+  constructor(private productService: ProductService) { }
 
   ngOnInit() {
+    this.productService.getProducts();
+    this.resetForm();
+  }
+
+  onSubmit(productForm: NgForm) {
+    this.productService.insertProduct(productForm.value);
+    this.resetForm(productForm);
+  }
+
+  resetForm(productForm?: NgForm) {
+    if(productForm != null) {
+      productForm.reset();
+      this.productService.selectProduct = new Product();
+    }
   }
 
 }
