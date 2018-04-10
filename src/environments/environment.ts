11e46251dff8dc3562db548558ba6e76cbcdc763
MOTORCREDITO\bastacio <b.astacio@motorcredito.com.do>
2018-04-10 10:33:19 -0700

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAPjkO64uy0GCdQwu3PXcG6PKXdvKRJGnI",
+    authDomain: "angular-firebase-2482c.firebaseapp.com",
+    databaseURL: "https://angular-firebase-2482c.firebaseio.com",
+    projectId: "angular-firebase-2482c",
+    storageBucket: "angular-firebase-2482c.appspot.com",
+    messagingSenderId: "617027009050"
+  }
 };
